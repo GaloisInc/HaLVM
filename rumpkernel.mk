1e7e1a2473ff7ef2e4e37d7c2157a33664c8cded
Adam Wick <awick@galois.com>
2016-01-21 14:21:52 +0000

    Checkpoint: builds a very simple example, but IO seems to block.

--- rumpkernel.mk
+++ rumpkernel.mk
@@ -10,23 +10,10 @@ RUMP_INCLUDE_DIR  := $(RUMP_STAGE_DIR)/include
 RUMPKERNEL_TARGET := $(RUMP_INCLUDE_DIR)/math.h
 
 $(RUMPKERNEL_TARGET):
-	(cd rumprun && ./build-rr.sh xen build)
+	(cd rumprun && ./build-rr.sh -d $(halvmlibdir)/rump xen build)
 
 install:: $(RUMPKERNEL_TARGET)
-	$(INSTALL) -D $(RUMP_STAGE_LIBDIR)/libc.a $(halvmlibdir)/rts/libc.a
-	$(INSTALL) -D $(RUMP_STAGE_LIBDIR)/libm.a $(halvmlibdir)/rts/libm.a
-	$(INSTALL) -D $(RUMP_STAGE_LIBDIR)/libpthread.a $(halvmlibdir)/rts/libpthread.a
-	$(INSTALL) -D $(RUMP_STAGE_LIBDIR)/rumprun-xen/librump.a $(halvmlibdir)/rts/librump.a
-	$(INSTALL) -D $(RUMP_STAGE_LIBDIR)/rumprun-xen/librumprun_base.a $(halvmlibdir)/rts/librumprun_base.a
-	$(INSTALL) -D $(RUMP_STAGE_LIBDIR)/rumprun-xen/rumprun.o $(halvmlibdir)/rts/rumprun.o
-	$(INSTALL) -D $(RUMP_STAGE_LIBDIR)/rumprun-xen/librumprun_base.a $(halvmlibdir)/rts/librumprun_base.a
-	$(INSTALL) -D $(RUMP_STAGE_LIBDIR)/rumprun-xen/librumpvfs.a $(halvmlibdir)/rts/librumpvfs.a
-	$(INSTALL) -D $(RUMP_STAGE_LIBDIR)/rumprun-xen/librumpkern_bmktc.a $(halvmlibdir)/rts/librumpkern_bmktc.a
-	$(INSTALL) -D $(RUMP_STAGE_LIBDIR)/rumprun-xen/librumpdev.a $(halvmlibdir)/rts/librumpdev.a
-	$(INSTALL) -D $(RUMP_STAGE_LIBDIR)/rumprun-xen/librumpfs_tmpfs.a $(halvmlibdir)/rts/librumpfs_tmpfs.a
-	$(INSTALL) -D $(RUMP_STAGE_LIBDIR)/rumprun-xen/librumpnet_config.a $(halvmlibdir)/rts/librumpnet_config.a
-	$(INSTALL) -D $(RUMP_STAGE_LIBDIR)/rumprun-xen/librumpnet_net.a $(halvmlibdir)/rts/librumpnet_net.a
-	$(INSTALL) -D $(RUMP_STAGE_LIBDIR)/rumprun-xen/librumpnet.a $(halvmlibdir)/rts/librumpnet.a
-	$(INSTALL) -D $(RUMP_STAGE_LIBDIR)/rumprun-xen/librumpdev_bpf.a $(halvmlibdir)/rts/librumpdev_bpf.a
-	$(INSTALL) -D $(RUMP_STAGE_LIBDIR)/rumprun-xen/librumpdev_vnd.a $(halvmlibdir)/rts/librumpdev_vnd.a
-	$(INSTALL) -D $(RUMP_STAGE_LIBDIR)/rumprun-xen/librumpdev_rnd.a $(halvmlibdir)/rts/librumpdev_rnd.a
+	(cd rumprun && ./build-rr.sh -d $(halvmlibdir)/rump xen install)
+
+install:: $(RUMPKERNEL_TARGET)
+	$(INSTALL) -D $(RUMP_STAGE_LIBDIR)/xen.ldscript $(halvmlibdir)/xen.ldscript
