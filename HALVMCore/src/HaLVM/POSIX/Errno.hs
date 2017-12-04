698acb710f1950390f2ecc9923d0926d6c0a70b6
Adam Wick <awick@uhsure.com>
2017-12-03 18:03:45 -0800

    Turns out, syscalls return -errnos. Who knew?

--- HALVMCore/src/HaLVM/POSIX/Errno.hs
+++ HALVMCore/src/HaLVM/POSIX/Errno.hs
@@ -10,17 +10,13 @@ import Foreign.C.Error(Errno(..),eINTR)
 import Foreign.C.Types(CInt(..))
 
 errnoReturn :: Num a => Errno -> IO a
-errnoReturn err =
-  do setErrno err
-     return (-1)
+errnoReturn (Errno v) = return (fromIntegral (- v))
 
-runWithEINTR :: Num b => IO b -> IO b
+runWithEINTR :: Num a => IO a -> IO a
 runWithEINTR = handle nevermind
  where
   nevermind :: Num a => SomeException -> IO a
-  nevermind _  = do setErrno eINTR
-                    return (-1)
-
+  nevermind _  = errnoReturn eINTR
 
 setErrno :: Errno -> IO ()
 setErrno (Errno v) = set_errno v
